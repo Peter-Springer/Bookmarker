ddcd2cecb4b46172563bb8c2a85938c16f22bc03
Peter Springer <peterspringer829@gmail.com>
2016-07-13 14:39:15 -0600

    phase three complete

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+var bookmarkCount = 0;
+var readBookmarkCount = 0;
+var unreadBookmarkCount = 0;
+
+$('.url-input-js').on('keyup', function() {
+  var url = $('.url-input-js').val();
+  var title = $('.text-input-js').val();
+  if (url !== "" && title !== "") {
+    $('#submit-js').prop('disabled', false);
+  } else {
+    $('#submit-js').prop('disabled', true);
+  }
+ });
 
 $('#submit-js').on('click', function(event) {
   event.preventDefault();
@@ -11,17 +24,33 @@ $('#submit-js').on('click', function(event) {
     $('.error-message-area-js').remove();
   }
   var list = document.createElement('li');
-  $(list).append(title);
+  $(list).append("<h4>" + title + "</h4>");
   $(list).append('</br>' + "<a href='" + url + "' target='_blank'>" + url + "</a>");
   $(list).append('</br>' + '<button class="read-button-js">READ</button>');
   $(list).append('<button class="remove-button-js">REMOVE</button>');
   $('.bookmark-output-js').append(list);
+  bookmarkCount = $('li').length;
+  $('.count-js').replaceWith('<p class="count-js">' + "Bookmarks in Inventory :" + bookmarkCount + '</p>');
+  unreadBookmarkCount = bookmarkCount - readBookmarkCount;
+  $('.unread-count-js').replaceWith('<p class="unread-count-js">' + "Unread Bookmarks :" + unreadBookmarkCount + '</p>')
 });
 
 $('.bookmark-output-js').on('click', '.read-button-js', function() {
   $(this).parent().toggleClass('read');
+  readBookmarkCount = $('.read').length;
+  $('.read-count-js').replaceWith('<p class="read-count-js">' + "Read Bookmarks :" + readBookmarkCount + '</p>')
+  unreadBookmarkCount = bookmarkCount - readBookmarkCount;
+  $('.unread-count-js').replaceWith('<p class="unread-count-js">' + "Unread Bookmarks :" + unreadBookmarkCount + '</p>')
 });
 
 $('.bookmark-output-js').on('click', '.remove-button-js', function() {
   $(this).parent().remove();
+  bookmarkCount = $('li').length;
+  $('.count-js').replaceWith('<p class="count-js">' + "Bookmarks in Inventory :" + bookmarkCount + '</p>');
+  readBookmarkCount = $('.read').length;
+  $('.read-count-js').replaceWith('<p class="read-count-js">' + "Read Bookmarks :" + readBookmarkCount + '</p>')
+  unreadBookmarkCount = bookmarkCount - readBookmarkCount;
+  $('.unread-count-js').replaceWith('<p class="unread-count-js">' + "Unread Bookmarks :" + unreadBookmarkCount + '</p>')
 });
+
+// var count = document.createElement('p')
